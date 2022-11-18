35ec665626620c61224b8d6e60a9aa9a9dd0cb9f
shasahasr <shasahasr@gmail.com>
2022-11-17 18:28:07 -0600

    tried firebase again

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,18 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import { initializeApp } from "firebase/app";
-
+import firebase from "firebase/app"
+import "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDigrMGJJZhWN0PoQN8-C2wDHSh1foqEzo",
-  authDomain: "shsweb-24e85.firebaseapp.com",
-  projectId: "shsweb-24e85",
-  storageBucket: "shsweb-24e85.appspot.com",
-  messagingSenderId: "114968129475",
-  appId: "1:114968129475:web:127469dcf59a6301415776",
-  measurementId: "G-G5S9D4S74J"
-}
+    apiKey: "AIzaSyDigrMGJJZhWN0PoQN8-C2wDHSh1foqEzo",
+    authDomain: "shsweb-24e85.firebaseapp.com",
+    projectId: "shsweb-24e85",
+    storageBucket: "shsweb-24e85.appspot.com",
+    messagingSenderId: "114968129475",
+    appId: "1:114968129475:web:8f22327c2ca31e82415776",
+    measurementId: "G-0KCRM518N9"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
-
-export let db = firebase.firestore();
-
+  export const auth = firebase.auth();
+  export const googleProvider = new firebase.auth.GoogleAuthProvider();
